6af63b00e2c35133e4b11ba58358d838336eda84
AJAY627 <ajay41527@gmail.com>
2021-06-19 22:48:39 +0530

    Add sign in to profile when right id entered

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,20 @@ const User = require('../models/user');
 
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title: 'User Profile'
-    });
+    if(req.cookies.user_id){
+         User.findById(req.cookies.user_id,function(err,user){
+             if(user){
+                 return res.render('user_profile',{
+                     title: "User profile",
+                     user: user
+                 })
+             }
+             return res.redirect('/users/sign-in')
+         });
+    
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 //render the sign up pages
 module.exports.signUp = function(req,res){
@@ -46,5 +57,29 @@ module.exports.create = function(req,res){
 
 //sign in and create session of the user
 module.exports.createSession = function(req,res){
+    
+      // steps to authenticate
+    // find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing in'); return}
+        // handle user found
+        if (user){
 
+            // handle password which doesn't match
+            if (user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            // handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            // handle user not found
+
+            return res.redirect('back');
+        }
+
+
+    });
 }
