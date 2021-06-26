c756f4924844346953736326899eac57391f952d
AJAY627 <ajay41527@gmail.com>
2021-06-26 08:14:19 +0530

    Check Authentication on Creating Post

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 console.log('router loaded');
+
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+router.use('/posts', require('./posts'));
 
 module.exports = router;
